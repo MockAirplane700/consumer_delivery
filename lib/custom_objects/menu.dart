48f24e4a45a6a04c15f6ddca92bebe8a9aba235b
siziba <sizibamthandazo@yahoo.com>
2022-05-07 06:33:09 -0500

    Initial commit

---
+++ lib/custom_objects/menu.dart
@@ -0,0 +1,23 @@
+// todo: create a menu object that holds the relevent information to make a selection
+
+class Menu {
+  //the string elements of the object
+  String network_image = '', name_of_dish = '',description = '',special_instructions= '';
+  double  price = 0.00;
+
+   Menu(String network_image, String name_of_dish,double price,String description,String special_instructions){
+      this.network_image = network_image;
+      this.name_of_dish = name_of_dish;
+      this.description = description;
+      this.special_instructions = special_instructions;
+      this.price = price;
+
+  }
+
+  //getter and setter methods
+  double get_price() {
+     return price;
+  }
+
+
+}
